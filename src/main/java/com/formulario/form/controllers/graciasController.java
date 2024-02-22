35c44c1a659e909adc0445a6b0c609440c42c3de
Luisa vargas <luchisvargasgomez@gmail.com>
2024-02-22 17:09:34 -0500

    arreglamos los urls

--- src/main/java/com/formulario/form/controllers/graciasController.java
+++ src/main/java/com/formulario/form/controllers/graciasController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/grupo16/form")
 public class graciasController {
 
-    @GetMapping("/grupo16/gracias")
+    @GetMapping("/gracias")
     public String gracias() {
         return ("gracias.html");
     }
